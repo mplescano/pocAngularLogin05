d68ce8388f1a918fcdb1303fec92fc1e65cbd1a2
mlescano <Mack.Lescano_avantica@scotiabank.com.pe>
2019-01-29 17:12:16 -0500

    Adding CRUD

--- src/main/webapp/app/users/users.module.ts
+++ src/main/webapp/app/users/users.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AuthenticationService} from './authentication.service';
 import {UserService} from './user.service';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UserListComponent, UserFormComponent],
   imports: [
     CommonModule
   ],
